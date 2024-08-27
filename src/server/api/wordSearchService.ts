0862a37dc4461b2ca01d3601e99090b6868accbf
LloydElery <dennisgooglar@gmail.com>
2024-08-27 16:43:21 +0200

    - Added logic to handle search

--- src/server/api/wordSearchService.ts
+++ src/server/api/wordSearchService.ts
@@ -2,15 +2,17 @@
 
 import { normalizeWord } from "~/utils/wordUtils";
 import { db, words } from "../db";
-import { eq } from "drizzle-orm";
+import { sql } from "drizzle-orm";
 
 export async function searchWordsWithLetters(letters: string) {
-  const normalizedLetters = normalizeWord(letters);
+  const normalizedLetters = normalizeWord(letters).toLowerCase();
 
   const result = await db
-    .select()
+    .selectDistinct({ word: sql<string>`lower(${words.word})` })
     .from(words)
-    .where(eq(words.normalized_word, normalizedLetters));
+    .where(
+      sql`${normalizedLetters} ILIKE '%' || ${words.normalized_word} || '%'`,
+    );
 
   return result.map((row) => row.word);
 }
