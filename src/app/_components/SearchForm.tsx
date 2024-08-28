b70a0bc3f7d4853bef08581d2909d50cb19f466b
LloydElery <dennisgooglar@gmail.com>
2024-08-28 09:59:35 +0200

    - Created a searchform for users to be able to use

---
+++ src/app/_components/SearchForm.tsx
@@ -0,0 +1,35 @@
+"use client";
+import { useState } from "react";
+import { useSearch } from "../hooks/useSearch";
+
+const SearchForm = () => {
+  const [query, setQuery] = useState("");
+  const { results, search } = useSearch();
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    search(query);
+  };
+
+  return (
+    <>
+      <form onSubmit={handleSubmit}>
+        <input
+          className="text-black"
+          type="text"
+          value={query}
+          onChange={(e) => setQuery(e.target.value)}
+          placeholder="SÖK ORD"
+        />
+        <button type="submit">Sök</button>
+        <ul>
+          {results.map((word, index) => (
+            <li key={index}>{word}</li>
+          ))}
+        </ul>
+      </form>
+    </>
+  );
+};
+
+export default SearchForm;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
+import SearchForm from "./_components/SearchForm";
+
 // Automaticly updates the deployed project whenever content in the db updates
 export const dynamic = "force-dynamic";
 
 export default async function HomePage() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-      Search:
+      <SearchForm />
       <div className="flex flex-wrap justify-center gap-1"></div>
     </main>
   );
