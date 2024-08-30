5b58fb1ad56f8dfbaf8c85b6d432493ad39b3cda
LloydElery <dennisgooglar@gmail.com>
2024-08-30 08:48:17 +0200

    - Added an empty file for future crud functions - Moved a folder

--- src/server/migrations/add_pg_trgm_index.ts
+++ src/server/migrations/add_pg_trgm_index.ts
@@ -1,12 +1,10 @@
 import { sql } from "drizzle-orm";
-import { db } from "../server/db"; // Justera importvägen om det behövs
+import { db } from "../db";
 
 async function addPgTrgmIndex() {
   try {
-    // Skapa pg_trgm-extension om den inte redan finns
     await db.execute(sql`CREATE EXTENSION IF NOT EXISTS pg_trgm;`);
 
-    // Skapa ett trigram-index på kolumnen normalized_word
     await db.execute(sql`
       CREATE INDEX IF NOT EXISTS trgm_idx
       ON words USING GIST (normalized_word gist_trgm_ops);
@@ -18,5 +16,4 @@ async function addPgTrgmIndex() {
   }
 }
 
-// Kör migrationen
 addPgTrgmIndex();
---
+++ src/server/queries.ts
@@ -0,0 +1,1 @@
+// Queries to manipulate database
