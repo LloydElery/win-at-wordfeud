e24c84d1a9a5aaa12e8589195fe28542f63ccb7b
LloydElery <dennisgooglar@gmail.com>
2024-08-30 16:51:18 +0200

    - Added a value to each letter

---
+++ src/utils/calculateLetterValue.ts
@@ -0,0 +1,40 @@
+import { normalizeWord } from "~/server/api/wordSearchService";
+
+const letterValues: Record<string, number> = {
+  a: 1,
+  b: 3,
+  c: 8,
+  d: 1,
+  e: 1,
+  f: 3,
+  g: 2,
+  h: 3,
+  i: 1,
+  j: 7,
+  k: 3,
+  l: 2,
+  m: 3,
+  n: 1,
+  o: 2,
+  p: 4,
+  q: 10,
+  r: 1,
+  s: 1,
+  t: 1,
+  u: 4,
+  v: 3,
+  w: 8,
+  x: 8,
+  y: 7,
+  z: 8,
+  å: 4,
+  ä: 4,
+  ö: 4,
+};
+
+export function calculateWordValue(word: string): number {
+  normalizeWord(word);
+  return word
+    .split("")
+    .reduce((total, char) => total + (letterValues[char] || 0), 0);
+}
