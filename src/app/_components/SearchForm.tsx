e54ade3f6c010d4b2561542f6abae88d261f7424
LloydElery <dennisgooglar@gmail.com>
2024-09-02 12:02:59 +0200

    - Replaced the toggle button with a radiobutton - Updated the sorting toggle function to return a boolean value - Added a function to sort by word value

--- src/app/_components/SearchForm.tsx
+++ src/app/_components/SearchForm.tsx
@@ -4,7 +4,7 @@ import { useSearch } from "../hooks/useSearch";
 
 const SearchForm = () => {
   const [query, setQuery] = useState("");
-  const { results, search, sortBy, setSortBy } = useSearch();
+  const { results, search, sortByValue, setSortByValue } = useSearch();
   console.log("results: ", results);
 
   const handleSubmit = (e: React.FormEvent) => {
@@ -12,29 +12,18 @@ const SearchForm = () => {
     search(query);
   };
 
-  const handleSortChange = (event: { target: { value: string } }) => {
+  const handleSortToggle = () => {
+    setSortByValue(!sortByValue);
+  };
+
+  /* const handleSortChange = (event: { target: { value: string } }) => {
     setSortBy(event.target.value as "length" | "value");
     console.log(sortBy);
-  };
+  }; */
 
   return (
     <>
       <form onSubmit={handleSubmit}>
-        <label className="switch">
-          <input
-            type="checkbox"
-            checked={sortBy === "length"}
-            onChange={(e) =>
-              handleSortChange({
-                target: { value: e.target.checked ? "length" : "value" },
-              })
-            }
-          />
-          <span className="slider">Sorterar efter</span>
-          <span className="label-text">
-            <strong>{sortBy === "length" ? "värde" : "längd"}</strong>
-          </span>
-        </label>
         <input
           className="text-black"
           type="text"
@@ -42,14 +31,22 @@ const SearchForm = () => {
           onChange={(e) => setQuery(e.target.value.toUpperCase())}
           placeholder="SÖK ORD"
         />
-
         <button type="submit">Sök</button>
       </form>
+
+      <label>
+        Sortera efter poäng
+        <input
+          type="checkbox"
+          checked={sortByValue}
+          onChange={handleSortToggle}
+        />
+      </label>
 
       <ul>
         {results.map((word, index) => (
           <li key={index}>
-            {word.word.toUpperCase()} : {word.value} poäng
+            {word.word.toUpperCase()} - {word.value}
           </li>
         ))}
       </ul>
--- src/app/hooks/useSearch.ts
+++ src/app/hooks/useSearch.ts
@@ -1,16 +1,17 @@
-import { useState } from "react";
+import { useMemo, useState } from "react";
 import { Word } from "../utils/WordInterface";
+import { sortByValueDesc } from "~/server/api/wordSearchService";
 
 export const useSearch = () => {
   const [results, setResults] = useState<Word[]>([]);
   const [loading, setLoading] = useState(false);
-  const [sortBy, setSortBy] = useState<"length" | "value">("length");
+  const [sortByValue, setSortByValue] = useState(false);
 
   const search = async (query: string) => {
     setLoading(true);
     try {
       const response = await fetch(
-        `/api/search?letters=${encodeURIComponent(query)}&sortBy=${sortBy}`,
+        `/api/search?letters=${encodeURIComponent(query)}`,
       );
       const data = await response.json();
       setResults(data.words || []);
@@ -21,5 +22,18 @@ export const useSearch = () => {
     }
   };
 
-  return { results, loading, search, sortBy, setSortBy };
+  const sortedResults = useMemo(() => {
+    if (sortByValue) {
+      return sortByValueDesc(results);
+    }
+    return results;
+  }, [results, sortByValue]);
+
+  return {
+    results: sortedResults,
+    loading,
+    search,
+    sortByValue,
+    setSortByValue,
+  };
 };
