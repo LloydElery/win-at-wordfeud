83ffc448990531068c095b294854fe796792a61a
LloydElery <dennisgooglar@gmail.com>
2024-09-05 06:59:58 +0200

    - working on tables

--- src/app/api/report/route.ts
+++ src/app/api/report/route.ts
@@ -1,18 +1,43 @@
-import { error } from "console";
+import { useAuth } from "@clerk/nextjs";
+import { clerkClient } from "@clerk/nextjs/server";
 import { NextRequest, NextResponse } from "next/server";
 import { reportWord } from "~/server/queries";
 
 export async function POST(req: NextRequest, res: NextResponse) {
-  const { word } = await req.json();
+  const { word, wordId } = await req.json();
+  const { userId } = useAuth();
 
-  if (!word || typeof word !== "string") {
-    return NextResponse.json({ error: "Word is required" }, { status: 400 });
+  if (
+    !word ||
+    typeof word !== "string" ||
+    !wordId ||
+    typeof wordId !== "number"
+  ) {
+    return NextResponse.json(
+      { error: "Word and Word ID is required" },
+      { status: 400 },
+    );
+  }
+
+  if (!userId) {
+    return NextResponse.json(
+      { error: "User must be logged in" },
+      { status: 401 },
+    );
   }
 
   try {
-    const updatedWord = await reportWord(word);
-    if (!updatedWord)
-      return NextResponse.json({ error: "Word not found" }, { status: 404 });
+    const user = await clerkClient.users.getUser(userId);
+    console.log("userId: ", user);
+
+    const updatedWord = await reportWord(user.id, wordId);
+    if (!updatedWord) {
+      return NextResponse.json(
+        { error: "Word already reported by user or not found" },
+        { status: 404 },
+      );
+    }
+
     return NextResponse.json(
       { message: `Reported word as not usable: ${word}` },
       { status: 200 },
--- src/server/queries.ts
+++ src/server/queries.ts
@@ -1,14 +1,29 @@
-import { eq, sql } from "drizzle-orm";
+import { and, eq, sql } from "drizzle-orm";
 import { db, words } from "./db";
+import { userReports } from "./db/schema";
 
 // UPDATE 'reports' to add 1 ontop of the current value
-export async function reportWord(word: string) {
+export async function reportWord(userId: string, wordId: number) {
+  const existingReport = await db
+    .select()
+    .from(userReports)
+    .where(and(eq(userReports.userId, userId), eq(userReports.wordId, wordId)));
+
+  if (existingReport.length > 0) return null;
+
   const result = await db
     .update(words)
     .set({ reports: sql`${words.reports} + 1` })
-    .where(eq(words.word, word))
+    .where(eq(words.id, wordId))
     .returning();
 
   if (result.length === 0) return null;
+
+  await db.insert(userReports).values({
+    userId,
+    wordId,
+    reportedAt: new Date(),
+  });
+
   return result[0];
 }
