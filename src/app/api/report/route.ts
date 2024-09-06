c7d77a24badd40ee9b1ab3ccb29701c9b91a7311
LloydElery <dennisgooglar@gmail.com>
2024-09-06 10:52:37 +0200

    - Added report function and connected it to the backend

--- src/app/api/report/route.ts
+++ src/app/api/report/route.ts
@@ -1,22 +1,19 @@
 import { useAuth } from "@clerk/nextjs";
-import { clerkClient } from "@clerk/nextjs/server";
+import { clerkClient, getAuth } from "@clerk/nextjs/server";
+import { error } from "console";
+import { eq } from "drizzle-orm";
 import { NextRequest, NextResponse } from "next/server";
+import { db, words } from "~/server/db";
 import { reportWord } from "~/server/queries";
 
-export async function POST(req: NextRequest, res: NextResponse) {
-  const { word, wordId } = await req.json();
-  const { userId } = useAuth();
+export async function POST(req: NextRequest) {
+  const { word } = await req.json();
+  console.log("word: ", word);
+  const { userId } = getAuth(req);
+  console.log("userId: ", userId);
 
-  if (
-    !word ||
-    typeof word !== "string" ||
-    !wordId ||
-    typeof wordId !== "number"
-  ) {
-    return NextResponse.json(
-      { error: "Word and Word ID is required" },
-      { status: 400 },
-    );
+  if (!word || typeof word !== "string") {
+    return NextResponse.json({ error: "Word is required" }, { status: 400 });
   }
 
   if (!userId) {
@@ -27,10 +24,22 @@ export async function POST(req: NextRequest, res: NextResponse) {
   }
 
   try {
-    const user = await clerkClient.users.getUser(userId);
-    console.log("userId: ", user);
+    const wordRecord = await db
+      .select({ id: words.id })
+      .from(words)
+      .where(eq(words.word, word));
 
-    const updatedWord = await reportWord(user.id, wordId);
+    console.log("wordRecord: ", wordRecord);
+
+    if (wordRecord.length === 0) {
+      return NextResponse.json({ error: "Word not found" }, { status: 404 });
+    }
+
+    const wordId = wordRecord[0]?.id;
+    console.log("wordId: ", wordId);
+
+    const updatedWord = await reportWord(userId, wordId!);
+
     if (!updatedWord) {
       return NextResponse.json(
         { error: "Word already reported by user or not found" },
