9e5de79c51bc2d46f6f6a4009af1663bd6218580
LloydElery <dennisgooglar@gmail.com>
2024-09-11 08:53:17 +0200

    - Created a header element with icons

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import { specialGridLayoutItems } from "./_components/gameboard/generateGameboard";
 import Grid from "./_components/gameboard/Grid";
+import Header from "./_components/Header";
 import SearchForm from "./_components/SearchForm";
 
 // Automaticly updates the deployed project whenever content in the db updates
@@ -7,7 +8,8 @@ export const dynamic = "force-dynamic";
 
 export default async function HomePage() {
   return (
-    <main className="bg-primaryBlue text-white">
+    <main className="min-h-screen bg-primaryBlue text-white">
+      <Header />
       <Grid size={15} specialGridItems={specialGridLayoutItems} />
       <SearchForm />
     </main>
