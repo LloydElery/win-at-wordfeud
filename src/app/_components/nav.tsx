332cfdfa3aab42d45655884e70a3229603a462ce
LloydElery <dennisgooglar@gmail.com>
2024-09-17 16:06:07 +0200

    - Added styling to nav-bar and letter tiles

--- src/app/_components/nav.tsx
+++ src/app/_components/nav.tsx
@@ -3,7 +3,7 @@ import { LetterTile } from "./LetterTile";
 
 export function Nav({ query }: { query: string }) {
   return (
-    <nav className="fixed bottom-0 flex w-full items-center justify-between p-4 text-xl font-semibold text-white">
+    <nav className="fixed bottom-0 flex w-full items-center justify-between bg-primaryBlue p-4 text-xl font-semibold text-white">
       <div className="letter-tiles flex">
         {query.split("").map((letter, index) => (
           <LetterTile key={index} letter={letter} />
