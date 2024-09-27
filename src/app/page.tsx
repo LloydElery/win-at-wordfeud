9c771bfc8b4564eb1efe81d6f371c32f40fc74a1
LloydElery <dennisgooglar@gmail.com>
2024-09-27 11:38:27 +0200

    - Created context to handle gameboard language across the applikation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import { specialGridLayoutItems } from "./_components/gameboard/generateGameboard";
 import Grid from "./_components/gameboard/Grid";
 import Header from "./_components/Header";
+import { GBLanguageProvider } from "./context/gameboard/GBLContext";
 import { ClientSideWrapper } from "./CSPage";
 
 // Automaticly updates the deployed project whenever content in the db updates
@@ -9,9 +10,11 @@ export const dynamic = "force-dynamic";
 export default async function HomePage() {
   return (
     <main className="min-h-screen bg-primaryBlue p-[7px] text-white">
-      <Header />
-      <ClientSideWrapper />
-      <Grid size={15} specialGridItems={specialGridLayoutItems} />
+      <GBLanguageProvider>
+        <Header />
+        <ClientSideWrapper />
+        <Grid size={15} specialGridItems={specialGridLayoutItems} />
+      </GBLanguageProvider>
     </main>
   );
 }
