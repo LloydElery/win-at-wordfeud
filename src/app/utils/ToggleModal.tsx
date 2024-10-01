0f70701c023efcd89583460e89391a03fe0c4e75
LloydElery <dennisgooglar@gmail.com>
2024-10-01 11:34:39 +0200

    - Fixed modal position

--- src/app/utils/ToggleModal.tsx
+++ src/app/utils/ToggleModal.tsx
@@ -47,21 +47,23 @@ const ToggleModal: React.FC<IToggleModal> = ({
       <div ref={iconRef} className={title + "_" + "icon"}>
         <IconComponent onClick={toggleModal} size={30} />
       </div>
-      <div>
+      <div className="icon-container">
         {isOpen ? (
-          <div ref={modalRef} className="modal-panel bg-modalGrey">
-            <div className="modal-header">
-              <h2 className="font-light">{title}</h2>
+          <div className="modal-container">
+            <div ref={modalRef} className="modal-panel bg-modalGrey">
+              <div className="modal-header">
+                <h2 className="font-light">{title}</h2>
 
-              <button
-                onClick={toggleModal}
-                className="close-button cursor-pointer bg-none"
-              >
-                X
-              </button>
-            </div>
-            <div className="modal-content">
-              <ContentComponent />
+                <button
+                  onClick={toggleModal}
+                  className="close-button cursor-pointer bg-none"
+                >
+                  X
+                </button>
+              </div>
+              <div className="modal-content">
+                <ContentComponent />
+              </div>
             </div>
           </div>
         ) : null}
