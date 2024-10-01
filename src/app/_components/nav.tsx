352bfa74061e4463df0419a0a33eea5d290914d8
LloydElery <dennisgooglar@gmail.com>
2024-10-01 14:28:31 +0200

    - Added styling to the letter tiles on the gameboard

--- src/app/_components/nav.tsx
+++ src/app/_components/nav.tsx
@@ -6,7 +6,15 @@ export function Nav({ query }: { query: string }) {
     <nav className="fixed bottom-0 flex w-full items-center justify-between bg-primaryBlue p-4 text-xl font-semibold text-white md:max-w-screen-md">
       <div className="letter-tiles flex">
         {query.split("").map((letter, index) => (
-          <LetterTile key={index} letter={letter} />
+          <div
+            className={
+              letter === " "
+                ? `blank-letter-tile border border-black bg-letterTile`
+                : `letter-tile bg-letterTile inner-border inner-border-black`
+            }
+          >
+            <LetterTile key={index} letter={letter} />
+          </div>
         ))}
       </div>
       <div>
