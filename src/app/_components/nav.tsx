19b135d909a9c7a559300efb3a56547819d2d535
LloydElery <dennisgooglar@gmail.com>
2024-10-09 10:16:47 +0200

    - Created a login button

--- src/app/_components/nav.tsx
+++ src/app/_components/nav.tsx
@@ -1,6 +1,6 @@
 import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
-import { LetterTile } from "./LetterTile";
 import LetterTiles from "./_ui/LetterTiles";
+import { AiOutlineUser } from "react-icons/ai";
 
 export function Nav({ query }: { query: string }) {
   return (
@@ -9,7 +9,11 @@ export function Nav({ query }: { query: string }) {
       <div className="nav-content-container">
         <div className="sign-in-and-profile z-20 flex items-center">
           <SignedOut>
-            <SignInButton />
+            <SignInButton>
+              <button className="rounded-full border border-letterTile bg-gameboardBG">
+                <AiOutlineUser size={30} />
+              </button>
+            </SignInButton>
           </SignedOut>
           <SignedIn>
             <UserButton />
