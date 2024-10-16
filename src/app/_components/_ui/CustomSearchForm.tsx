540caa015690572bca6b78d07bbde6f8251e1c21
LloydElery <dennisgooglar@gmail.com>
2024-10-16 13:46:25 +0200

    - Replaced focus with click

--- src/app/_components/_ui/CustomSearchForm.tsx
+++ src/app/_components/_ui/CustomSearchForm.tsx
@@ -23,7 +23,7 @@ const CustomSearchForm = forwardRef(
 
     const handleVirtualKeyboardVisibility = () => {
       if (document.visibilityState === "visible" && inputRef.current) {
-        inputRef.current.focus();
+        inputRef.current.click();
       }
     };
 
