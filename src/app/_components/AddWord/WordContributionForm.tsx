c7d4c0642b081370c4b029ad7b997c7059281eb8
LloydElery <dennisgooglar@gmail.com>
2024-10-17 11:48:29 +0200

    - Added logic to hanndle submissions from users to the "community-words" table. > Auth > Loading > Messages (true false) > input pattern

--- src/app/_components/AddWord/WordContributionForm.tsx
+++ src/app/_components/AddWord/WordContributionForm.tsx
@@ -1,28 +1,66 @@
 "use client";
-import { useState } from "react";
+import { useRef, useState } from "react";
 import GameboardLanguage from "../_ui/GameboardLanguage";
+import { useAuth } from "@clerk/nextjs";
 
 const WordContributionForm: React.FC = () => {
+  const { isLoaded, isSignedIn } = useAuth();
+  console.log("isLoaded: ", isLoaded, "isSignedIn: ", isSignedIn);
+
   const [word, setWord] = useState("");
+  const [message, setMessage] = useState<string | null>(null);
+  const [messageColor, setMessageColor] = useState<string | null>(null);
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  const inputRef = useRef<HTMLInputElement>(null);
 
   const handleSubmit = async (event: React.FormEvent) => {
     event.preventDefault();
-    const lowerCaeWord = word.trim().toLowerCase();
+
+    if (!isLoaded || isSubmitting) return;
+
+    if (!isSignedIn) {
+      setMessage("Logga in för att lägga till ord i vår databas");
+      setMessageColor("text-red-400");
+      return;
+    }
+
+    const lowerCaseWord = word.trim().toLowerCase();
+
+    setIsSubmitting(true);
 
     const res = await fetch("/api/add-word", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ word: lowerCaeWord }),
+      body: JSON.stringify({ word: lowerCaseWord }),
     });
 
     const result = await res.json();
+
     if (result.success) {
-      alert("Word submitted successfully!");
+      setMessage(`Tack! ${word.toUpperCase()} finns nu i vår databas.`);
+      setMessageColor("text-green-400");
     } else {
-      alert("Word already exists od failed to submit.");
+      setMessage(`${word.toUpperCase()} finns redan i vår databas.`);
+      setMessageColor("text-red-400");
     }
 
     setWord("");
+    setIsSubmitting(false);
+  };
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setWord(e.target.value);
+
+    const messageWhenWordDoesNotMatchFormat =
+      "Ord får enbart innehålla bokstäver i Svenska alfabetet";
+
+    if (inputRef.current) {
+      if (!e.target.validity.valid) {
+        inputRef.current.setCustomValidity(messageWhenWordDoesNotMatchFormat);
+      } else {
+        inputRef.current.setCustomValidity("");
+      }
+    }
   };
 
   return (
@@ -40,22 +78,29 @@ const WordContributionForm: React.FC = () => {
       <form onSubmit={handleSubmit}>
         <label className="flex gap-1 text-sm font-light text-letterTile">
           <input
+            ref={inputRef}
             className="rounded-md border border-gray-300 px-4 py-2 text-black"
             type="text"
-            pattern="[a-zåäöA-ZÅÄÖ]"
-            value={word}
-            onChange={(e) => setWord(e.target.value)}
+            pattern="[a-zåäöA-ZÅÄÖ]+"
+            value={word.toUpperCase()}
+            onChange={handleInputChange}
             placeholder="Skriv ditt ord"
             required
           />
+
           <button
             type="submit"
-            className="rounded bg-gray-100 px-4 py-2 text-gray-600"
+            className={`rounded bg-gray-100 px-4 py-2 text-gray-600 ${isSubmitting ? "cursor-not-allowed opacity-50" : ""}`}
+            disabled={isSubmitting}
           >
-            Skicka
+            {isSubmitting ? "Skickar..." : "Skicka"}
           </button>
         </label>
       </form>
+
+      <div className={`m-[7px] text-sm font-light ${messageColor}`}>
+        {message}
+      </div>
       <div className="gameboard-language-container">
         <GameboardLanguage />
       </div>
